9ad4af84c061e54b764d0c3a7a01a75d589b19d0
imac1901 <isidoramack@gmail.com>
2018-09-05 13:09:32 -0500

    Part 1 of 1.1.1

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Izzie Mack)
+ * @version (8/5/18)
+ */
+public class HelloWorld
+{
+    public static void main()
+    {
+        System.out.println("Hello World!");
+    
+    }
+}
